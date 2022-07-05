62a4ef3311b0c99edec957157531c5ca4d8c9489
Munzur Kolkiran <munzur56@gmail.com>
2022-07-05 08:25:06 +0300

    database guncellendi

--- base/views.py
+++ base/views.py
@@ -10,7 +10,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
-
 def registerPage(request):
     form = UserCreationForm()
 
--- study/settings.py
+++ study/settings.py
@@ -58,13 +58,22 @@ WSGI_APPLICATION = 'study.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'Mklkrn62.',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
